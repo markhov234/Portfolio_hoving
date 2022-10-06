3de73aa2593b9bdf8de99ee48a29cb6a7ba64678
Marc-Antoine Hovington <hovington234@gmail.com>
2022-10-05 22:14:25 -0400

    When clicking figure, generate a component withe the project

---
+++ src/Components/Organism/AccueilProject.js
@@ -0,0 +1,15 @@
+const AccueilProject = (props) => {
+  function isVisible(state) {
+    // https://medium.com/@thejasonfile/callback-functions-in-react-e822ebede766
+  }
+
+  return (
+    <div onClick={isVisible} className="o-accueil-project">
+      <div>
+
+      </div>
+    </div>
+  );
+};
+
+export default AccueilProject;
--- src/Components/Pages/AccueilNews.js
+++ src/Components/Pages/AccueilNews.js
@@ -1,44 +1,33 @@
-import folks from "../../Images/folks.png";
+// import folks from "../../Images/folks.png";
+import { useState } from "react";
+import AccueilProject from "./../Organism/AccueilProject";
 import AccueilBackInteractive from "./../Organism/AccueilBackInteractive";
 import ProjectsData from "./../../BackEnd/data.json";
 const AccueilNews = (props) => {
-  console.log(ProjectsData.projets[0]);
+  // console.log(ProjectsData.projets[0]);
+  const [isProjectVisible, setIsProjectVisible] = useState(false);
+  const [currentProject, setCurrentProject] = useState(null);
+
+  function projectSended(project) {
+    setIsProjectVisible((isVisible) => !isVisible);
+    setCurrentProject((project)=>project);
+  }
   return (
     <div className="o-accueil-realisation">
       <AccueilBackInteractive />
+      {isProjectVisible ? <AccueilProject isVisible={[isProjectVisible,setIsProjectVisible]} currentProject={currentProject} /> : ""}
       <div className="o-accueil-realisation-zone">
-        <figure className="o-accueil-realisation-figure">
-          <img alt="Site webs folks" src={folks}></img>
-          <div className="o-accueil-realisation-figcaption">
-            Oeuvre réalisé par{" "}
-            <span className="o-accueil-realisation-figcaption-span"></span>
-            Marc-Antoine Hovington
-          </div>
-        </figure>
-        <figure className="o-accueil-realisation-figure">
-          <img alt="" src="https://fakeimg.pl/250x250/"></img>
-          <div className="o-accueil-realisation-figcaption">
-            Oeuvre réalisé par{" "}
-            <span className="o-accueil-realisation-figcaption-span"></span>
-            Marc-Antoine Hovington
-          </div>
-        </figure>
-        <figure className="o-accueil-realisation-figure">
-          <img alt="" src="https://fakeimg.pl/250x250/"></img>
-          <div className="o-accueil-realisation-figcaption">
-            Oeuvre réalisé par{" "}
-            <span className="o-accueil-realisation-figcaption-span"></span>
-            Marc-Antoine Hovington
-          </div>
-        </figure>
-        <figure className="o-accueil-realisation-figure">
-          <img alt="" src="https://fakeimg.pl/250x250/"></img>
-          <div className="o-accueil-realisation-figcaption">
-            Oeuvre réalisé par{" "}
-            <span className="o-accueil-realisation-figcaption-span"></span>
-            Marc-Antoine Hovington
-          </div>
-        </figure>{" "}
+      {ProjectsData.projets.map((project, index) => (
+         <figure key={index} onClick={event => projectSended(project)} className="o-accueil-realisation-figure">
+         <img alt={project.name} src={`${project.image}`}></img>
+         <div className="o-accueil-realisation-figcaption">
+           Réalisé par 
+           <span className="o-accueil-realisation-figcaption-span"></span>
+           Marc-Antoine Hovington
+         </div>
+       </figure>
+        ))}
+
       </div>
     </div>
   );
