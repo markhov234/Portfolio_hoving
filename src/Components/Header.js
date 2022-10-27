16871d9524ecdcf9c53bb2ec60187e3e17e43bb1
MarkHov234 <hovington234@gmail.com>
2022-10-27 14:08:12 -0400

    menuv2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
-import Navigation from "./Organism/Navigation";
+// import Navigation from "./Organism/Navigation";
+import Navigation_v2 from "./Organism/Navigation_v2";
 const Header = (props) => {
   // Définis la function on Click ici
   return (
     <header className="o-header">
       <div className="o-header--content ">
-        <Navigation screenWidth={props.screenWidth}></Navigation>
+        <Navigation_v2 screenWidth={props.screenWidth}></Navigation_v2>
       </div>
     </header>
   );
