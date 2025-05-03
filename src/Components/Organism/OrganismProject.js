21fcfb133de1437f84881d642396c5df088e7a5d
MarkHov-Real <manhogward@hotmail.com>
2025-05-03 15:57:20 -0400

    removed AllIcons const

--- src/Components/Organism/OrganismProject.js
+++ src/Components/Organism/OrganismProject.js
@@ -1,7 +1,6 @@
 import ProjectsData from "./../../BackEnd/data.json";
 import { useState } from "react";
 import UpwardArrow from "../../Images/upward-arrow.png";
-import allIcons from "../ReactHook/importIconsImg";
 
 import AccueilRealisationSection from "./Accueil-Realisation-Section";
 
@@ -15,57 +14,6 @@ const OrganismProject = () => {
       <h2 className="o-accueil-realisation-zone-title">Mes projets</h2>
 
       {ProjectsData.projets.slice(0, visibleProjects).map((project, index) => (
-        // <div key={index} className={`o-accueil-realisation-project`}>
-        //   <h3 className="o-accueil-realisation-project-title">
-        //     {project.name}
-        //   </h3>
-        //   <ul className="o-accueil-realisation-project-techno-list">
-        //     {project.technologie.map((technologie, indexTechno) => (
-        //       <li
-        //         className="o-accueil-presentation-technos-item"
-        //         key={indexTechno}
-        //       >
-        //         <span className="o-accueil-presentation-technos-icon">
-        //           <img draggable="false" src={allIcons[technologie]} alt="" />
-        //         </span>
-        //         <p>{technologie}</p>
-        //       </li>
-        //     ))}
-        //   </ul>
-        //   <div className="o-accueil-realisation-project-description">
-        //     <p> {project.descriptionShort}</p>
-        //     <figure className="o-accueil-realisation-project-figure">
-        //       <img
-        //         className="o-accueil-realisation-project-img"
-        //         alt={project.name}
-        //         src={`${project.image.couverture}`}
-        //       ></img>
-        //     </figure>
-        //   </div>
-
-        //   <div className="o-accueil-realisation-project-button">
-        //     <div
-        //       onClick={() => console.log("allo")}
-        //       className="o-accueil-realisation-project-button-project"
-        //       id={project.id}
-        //     >
-        //       Consulter le projet
-        //     </div>
-        //     <a
-        //       href={`${
-        //         project.url_github
-        //           ? project.url_github
-        //           : "https://github.com/markhov234"
-        //       }`}
-        //       className="o-accueil-realisation-project-button-github"
-        //     >
-        //       <p>Consulter le github</p>
-        //       <span>
-        //         <img alt="arrow-up" src={UpwardArrow}></img>
-        //       </span>
-        //     </a>
-        //   </div>
-        // </div>
         <AccueilRealisationSection key={index} project={project} />
       ))}
       {visibleProjects < ProjectsData.projets.length && (
